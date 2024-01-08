48afba49aa80a1bf366c3ae79e7b3998b35bf832
Suuskehh <suus.harsveld@hva.nl>
2024-01-08 14:21:19 +0100

    serverless :(

--- .vercel/output/functions/render.func/Desktop/HVA/Github/i-love-web-app/dist/chunks/2023-11-24_o_koIv3n.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/2023-11-24_f8lK3bu2.mjs').then(n => n._);
-
-export { page };
